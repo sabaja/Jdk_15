e634d24c943353877890fdd6c9c79661d5421400
sabaja <jacoposabatini76@gmail.com>
2021-02-02 10:36:35 +0100

    ADD http2

--- src/main/java/com/works/jdk15/application/utils/StandardHTTP2.java
+++ src/main/java/com/works/jdk15/application/utils/StandardHTTP2.java
@@ -1,23 +1,32 @@
 package com.works.jdk15.application.utils;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.time.Duration;
 
+import static java.time.temporal.ChronoUnit.SECONDS;
+
+@Slf4j
 public class StandardHTTP2 {
 
     public static void main(String[] args) throws IOException, InterruptedException {
         HttpClient httpClient = HttpClient.newBuilder().build();
 
-        HttpRequest request =
+        HttpRequest getRequest =
                 HttpRequest.newBuilder()
                         .uri(URI.create("https://advancedweb.hu/"))
+                        .timeout(Duration.of(10, SECONDS))
                         .GET()
                         .build();
 
         HttpResponse<String> response =
-                httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+                httpClient.send(getRequest, HttpResponse.BodyHandlers.ofString());
+      log.info(String.valueOf(response));
+
     }
 }
