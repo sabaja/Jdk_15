317c2343526759e604ca227b2fa67d0a76fdbc16
sabaja <jacoposabatini76@gmail.com>
2021-06-29 17:19:46 +0200

    Add Httpclient

--- src/main/java/com/works/jdk15/application/utils/StreamImprovements.java
+++ src/main/java/com/works/jdk15/application/utils/StreamImprovements.java
@@ -1,6 +1,19 @@
 package com.works.jdk15.application.utils;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLParameters;
+import java.io.IOException;
+import java.net.Authenticator;
+import java.net.CookieHandler;
+import java.net.ProxySelector;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
 import java.util.Collections;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -11,6 +24,68 @@ import java.util.stream.Stream;
 public interface StreamImprovements {
 
     static void main(String[] args) {
+
+        HttpClient httpClient = new HttpClient() {
+            @Override
+            public Optional<CookieHandler> cookieHandler() {
+                return Optional.empty();
+            }
+
+            @Override
+            public Optional<Duration> connectTimeout() {
+                return Optional.empty();
+            }
+
+            @Override
+            public Redirect followRedirects() {
+                return null;
+            }
+
+            @Override
+            public Optional<ProxySelector> proxy() {
+                return Optional.empty();
+            }
+
+            @Override
+            public SSLContext sslContext() {
+                return null;
+            }
+
+            @Override
+            public SSLParameters sslParameters() {
+                return null;
+            }
+
+            @Override
+            public Optional<Authenticator> authenticator() {
+                return Optional.empty();
+            }
+
+            @Override
+            public Version version() {
+                return null;
+            }
+
+            @Override
+            public Optional<Executor> executor() {
+                return Optional.empty();
+            }
+
+            @Override
+            public <T> HttpResponse<T> send(HttpRequest request, HttpResponse.BodyHandler<T> responseBodyHandler) throws IOException, InterruptedException {
+                return null;
+            }
+
+            @Override
+            public <T> CompletableFuture<HttpResponse<T>> sendAsync(HttpRequest request, HttpResponse.BodyHandler<T> responseBodyHandler) {
+                return null;
+            }
+
+            @Override
+            public <T> CompletableFuture<HttpResponse<T>> sendAsync(HttpRequest request, HttpResponse.BodyHandler<T> responseBodyHandler, HttpResponse.PushPromiseHandler<T> pushPromiseHandler) {
+                return null;
+            }
+        };
         //The method dropWhile() drops all the elements of the stream until the given predicate fails.
         // When the stream is unordered, the dropWhile() drops all the elements until the given predicate fails
         Stream<Integer> integerStream = Stream.of(11, 22, 40, 60, 10, 15, 30, 100);
